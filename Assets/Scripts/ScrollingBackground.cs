51753e470e1bc3cda936909d7f39fda13ba68808
her17138 <her17138@uvg.edu.gt>
2018-02-10 16:19:05 -0600

    + accelerometer

---
+++ Assets/Scripts/ScrollingBackground.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingBackground : MonoBehaviour {
+
+	public float scrollingSpeed = 5f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (new Vector3 (0, -1, 0) * scrollingSpeed * Time.deltaTime);
+		if (transform.position.y >12f) {
+			transform.position = new Vector3 (transform.position.x, 12f, transform.position.z); 
+		}
+	}
+}
--- Assets/Scripts/Spaceship.cs
+++ Assets/Scripts/Spaceship.cs
@@ -3,23 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Spaceship : MonoBehaviour {
-	Rigidbody2D rb2d;
+	Rigidbody2D rb;
 	public Camera cam;
+	float speed = 0.05f;
 
 	// Use this for initialization
 	void Start () {
-		rb2d = GetComponent<Rigidbody2D>();
-		cam.transform.position = new Vector3(rb2d.transform.position.x, cam.transform.position.y, cam.transform.position.z);
+		rb = GetComponent<Rigidbody2D>();
+		cam.transform.position = new Vector3(rb.transform.position.x, cam.transform.position.y, cam.transform.position.z);
 		//anim = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
 	void Update()
 	{
-		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 7.0f;
-		transform.Translate(x, 0, 0);
-		var y = Input.GetAxis("Vertical") * Time.deltaTime * 7.0f;
-		transform.Translate(0, y, 0);
+		//Estos vectores dictan los límites de la pantalla, así el personaje no puede salirse de ella. 
+		Vector3 minScreenBounds = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 0));
+		Vector3 maxScreenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+
+		transform.Translate (Mathf.Clamp(Input.acceleration.x, minScreenBounds.x +1, maxScreenBounds.x -1), 0, 0);
+
+		float movX = Mathf.Clamp(Input.acceleration.x, minScreenBounds.x +1, maxScreenBounds.x -1); 
+		rb.transform.Translate(Vector2.right * speed * movX * Time.deltaTime);
+		cam.transform.position = new Vector3(rb.transform.position.x, cam.transform.position.y, cam.transform.position.z);
+
+
 	}
 
 }
